df082fdfef20b10a6addd824c2f36b12c4fc6286
arrathilar <wervvolf@gmail.com>
2020-06-17 02:11:23 +0300

    add dependencies

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
-#include <glm/glm.hpp>
+
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/transform.hpp>
+
+#include <stb_image.h>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include "Shader.h"
+#include "Sphere.h"
+#include "Camera.h"
+
+#include <cstdlib>
 #include <iostream>
+#include <vector>
+#include <map>
+#include <wtypes.h>
+#include <ctime>
+#include <filesystem>
+#include <string>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 int main() {
     if(!glfwInit()) {
